eb811b0c936689d933fc4cb8b39b7a456cf1e3e0
Christoph Buchner <bilderbuchi@phononoia.at>
2013-09-01 19:07:24 +0200

    Wrap tests in classed for more organization.

--- tests/test_help.py
+++ tests/test_help.py
@@ -3,41 +3,39 @@ import os
 from util_functions import SCRIPT_LOC, script_cmd
 
 
-def test_help(capfd):
-	"""Test if help text gets printed"""
-	script_cmd(SCRIPT_LOC + ' --help', os.getcwd())
-	out, err = capfd.readouterr()
-	assert out.startswith('usage: ofStateManager.py [-h]')
-	assert err == ''
+class TestHelp:
+	"""Test help subcommand functions"""
+	def test_help(self, capfd):
+		"""Test if help text gets printed"""
+		script_cmd(SCRIPT_LOC + ' --help', os.getcwd())
+		out, err = capfd.readouterr()
+		assert out.startswith('usage: ofStateManager.py [-h]')
+		assert err == ''
 
+	def test_help_record(self, capfd):
+		"""Test if record help text gets printed"""
+		script_cmd(SCRIPT_LOC + ' record -h', os.getcwd())
+		out, err = capfd.readouterr()
+		assert out.startswith('usage: ofStateManager.py record [-h]')
+		assert err == ''
 
-def test_help_record(capfd):
-	"""Test if record help text gets printed"""
-	script_cmd(SCRIPT_LOC + ' record -h', os.getcwd())
-	out, err = capfd.readouterr()
-	assert out.startswith('usage: ofStateManager.py record [-h]')
-	assert err == ''
+	def test_help_checkout(self, capfd):
+		"""Test if checkout help text gets printed"""
+		script_cmd(SCRIPT_LOC + ' checkout --help', os.getcwd())
+		out, err = capfd.readouterr()
+		assert out.startswith('usage: ofStateManager.py checkout [-h]')
+		assert err == ''
 
+	def test_help_archive(self, capfd):
+		"""Test if archive help text gets printed"""
+		script_cmd(SCRIPT_LOC + ' archive -h', os.getcwd())
+		out, err = capfd.readouterr()
+		assert out.startswith('usage: ofStateManager.py archive [-h]')
+		assert err == ''
 
-def test_help_checkout(capfd):
-	"""Test if checkout help text gets printed"""
-	script_cmd(SCRIPT_LOC + ' checkout --help', os.getcwd())
-	out, err = capfd.readouterr()
-	assert out.startswith('usage: ofStateManager.py checkout [-h]')
-	assert err == ''
-
-
-def test_help_archive(capfd):
-	"""Test if archive help text gets printed"""
-	script_cmd(SCRIPT_LOC + ' archive -h', os.getcwd())
-	out, err = capfd.readouterr()
-	assert out.startswith('usage: ofStateManager.py archive [-h]')
-	assert err == ''
-
-
-def test_help_list(capfd):
-	"""Test if list help text gets printed"""
-	script_cmd(SCRIPT_LOC + ' list --help', os.getcwd())
-	out, err = capfd.readouterr()
-	assert out.startswith('usage: ofStateManager.py list [-h]')
-	assert err == ''
+	def test_help_list(self, capfd):
+		"""Test if list help text gets printed"""
+		script_cmd(SCRIPT_LOC + ' list --help', os.getcwd())
+		out, err = capfd.readouterr()
+		assert out.startswith('usage: ofStateManager.py list [-h]')
+		assert err == ''
--- tests/test_record.py
+++ tests/test_record.py
@@ -25,94 +25,87 @@ from util_functions import SCRIPT_LOC, REPLAY_DIR, script_cmd, load_json_file
 
 
 @pytest.mark.usefixtures('set_up')
-#class TestRecord:
-#	"""Test record subcommand functions"""
-def test_record():
-	ret = script_cmd(SCRIPT_LOC + ' record -p mockProject', os.getcwd())
-	assert ret == 0
-	std = load_json_file(os.path.join(REPLAY_DIR, 'md_record.json'))
-	test = load_json_file(os.path.join('mockProject', 'metadata.json'))
-	assert test == std
+class TestRecord:
+	"""Test record subcommand functions"""
+	def test_record(self):
+		ret = script_cmd(SCRIPT_LOC + ' record -p mockProject', os.getcwd())
+		assert ret == 0
+		std = load_json_file(os.path.join(REPLAY_DIR, 'md_record.json'))
+		test = load_json_file(os.path.join('mockProject', 'metadata.json'))
+		assert test == std
 
+	def test_record_remotedir(self):
+		currentdir = os.getcwd()
+		os.chdir(os.path.dirname(SCRIPT_LOC))
+		ret = script_cmd(os.path.join('./', os.path.basename(SCRIPT_LOC)) +
+							' record -p ' + os.path.join(currentdir, 'mockProject'),
+						os.getcwd())
+		assert ret == 0
+		std = load_json_file(os.path.join(REPLAY_DIR, 'md_record.json'))
+		test = load_json_file(os.path.join(currentdir, 'mockProject',
+										'metadata.json'))
+		assert test == std
 
-def test_record_remotedir():
-	currentdir = os.getcwd()
-	os.chdir(os.path.dirname(SCRIPT_LOC))
-	ret = script_cmd(os.path.join('./', os.path.basename(SCRIPT_LOC)) +
-						' record -p ' + os.path.join(currentdir, 'mockProject'),
-					os.getcwd())
-	assert ret == 0
-	std = load_json_file(os.path.join(REPLAY_DIR, 'md_record.json'))
-	test = load_json_file(os.path.join(currentdir, 'mockProject',
-									'metadata.json'))
-	assert test == std
+	def test_record_default_p_dir(self):
+		currentdir = os.getcwd()
+		os.chdir('mockProject')
+		ret = script_cmd(SCRIPT_LOC + ' record', os.getcwd())
+		assert ret == 0
+		std = load_json_file(os.path.join(REPLAY_DIR, 'md_record.json'))
+		test = load_json_file(os.path.join(currentdir, 'mockProject',
+										'metadata.json'))
+		assert test == std
 
+	def test_record_named(self, capfd):
+		ret = script_cmd(SCRIPT_LOC + ' record -p mockProject -n snapshot-1',
+						os.getcwd())
+		assert ret == 0
+		out, err = capfd.readouterr()
+		assert 'DEBUG' not in out
+		assert 'DEBUG' not in err
+		std = load_json_file(os.path.join(REPLAY_DIR, 'md_record_snapshot-1.json'))
+		test = load_json_file(os.path.join('mockProject', 'metadata.json'))
+		assert test == std
 
-def test_record_default_p_dir():
-	currentdir = os.getcwd()
-	os.chdir('mockProject')
-	ret = script_cmd(SCRIPT_LOC + ' record', os.getcwd())
-	assert ret == 0
-	std = load_json_file(os.path.join(REPLAY_DIR, 'md_record.json'))
-	test = load_json_file(os.path.join(currentdir, 'mockProject',
-									'metadata.json'))
-	assert test == std
+	def test_record_verbosity(self, capfd):
+		ret = script_cmd(SCRIPT_LOC + ' record -v -p mockProject -n snapshot-1',
+						os.getcwd())
+		assert ret == 0
+		out, _ = capfd.readouterr()
+		assert 'DEBUG' in out
 
+		std = load_json_file(os.path.join(REPLAY_DIR, 'md_record_snapshot-1.json'))
+		test = load_json_file(os.path.join('mockProject', 'metadata.json'))
+		assert test == std
 
-def test_record_named(capfd):
-	ret = script_cmd(SCRIPT_LOC + ' record -p mockProject -n snapshot-1',
-					os.getcwd())
-	assert ret == 0
-	out, err = capfd.readouterr()
-	assert 'DEBUG' not in out
-	assert 'DEBUG' not in err
-	std = load_json_file(os.path.join(REPLAY_DIR, 'md_record_snapshot-1.json'))
-	test = load_json_file(os.path.join('mockProject', 'metadata.json'))
-	assert test == std
+	def test_record_wrong_project(self, capfd):
+		ret = script_cmd(SCRIPT_LOC + ' record -p mockProjectWRONG', os.getcwd())
+		assert ret == 1
+		_, err = capfd.readouterr()
+		# Do this because subprocess does not raise its child's exceptions
+		assert 'OSError: [Errno 2]' in err
 
+	def test_record_update(self):
+		# copy snapshot-1 metadata file over to mockProject
+		shutil.copyfile(os.path.join(REPLAY_DIR, 'md_record_snapshot-1.json'),
+						os.path.join('mockProject', 'metadata.json'))
+		# this has to bail because -u was not given
+		ret = script_cmd(SCRIPT_LOC + ' record -p mockProject -n snapshot-1',
+						os.getcwd())
+		assert ret == 1
+		# this has to work
+		ret = script_cmd(SCRIPT_LOC + ' record -u -p mockProject -n snapshot-1',
+						os.getcwd())
+		assert ret == 0
 
-def test_record_verbosity(capfd):
-	ret = script_cmd(SCRIPT_LOC + ' record -v -p mockProject -n snapshot-1',
-					os.getcwd())
-	assert ret == 0
-	out, _ = capfd.readouterr()
-	assert 'DEBUG' in out
+		std = load_json_file(os.path.join(REPLAY_DIR, 'md_record_snapshot-1.json'))
+		test = load_json_file(os.path.join('mockProject', 'metadata.json'))
+		assert test == std
 
-	std = load_json_file(os.path.join(REPLAY_DIR, 'md_record_snapshot-1.json'))
-	test = load_json_file(os.path.join('mockProject', 'metadata.json'))
-	assert test == std
-
-
-def test_record_wrong_project(capfd):
-	ret = script_cmd(SCRIPT_LOC + ' record -p mockProjectWRONG', os.getcwd())
-	assert ret == 1
-	_, err = capfd.readouterr()
-	# Do this because subprocess does not raise its child's exceptions
-	assert 'OSError: [Errno 2]' in err
-
-
-def test_record_update():
-	# copy snapshot-1 metadata file over to mockProject
-	shutil.copyfile(os.path.join(REPLAY_DIR, 'md_record_snapshot-1.json'),
-					os.path.join('mockProject', 'metadata.json'))
-	# this has to bail because -u was not given
-	ret = script_cmd(SCRIPT_LOC + ' record -p mockProject -n snapshot-1',
-					os.getcwd())
-	assert ret == 1
-	# this has to work
-	ret = script_cmd(SCRIPT_LOC + ' record -u -p mockProject -n snapshot-1',
-					os.getcwd())
-	assert ret == 0
-
-	std = load_json_file(os.path.join(REPLAY_DIR, 'md_record_snapshot-1.json'))
-	test = load_json_file(os.path.join('mockProject', 'metadata.json'))
-	assert test == std
-
-
-def test_record_description():
-	ret = script_cmd(SCRIPT_LOC + ' record -p mockProject' +
-					' -d "My Test description"', os.getcwd())
-	assert ret == 0
-	std = load_json_file(os.path.join(REPLAY_DIR, 'md_record_description.json'))
-	test = load_json_file(os.path.join('mockProject', 'metadata.json'))
-	assert test == std
+	def test_record_description(self):
+		ret = script_cmd(SCRIPT_LOC + ' record -p mockProject' +
+						' -d "My Test description"', os.getcwd())
+		assert ret == 0
+		std = load_json_file(os.path.join(REPLAY_DIR, 'md_record_description.json'))
+		test = load_json_file(os.path.join('mockProject', 'metadata.json'))
+		assert test == std
