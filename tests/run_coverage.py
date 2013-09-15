6464a84c081ba01dbcf1145c598889ac4bfb0d46
Christoph Buchner <bilderbuchi@phononoia.at>
2013-09-15 19:26:24 +0200

    Pass command line arguments to py.test.

--- tests/run_coverage.py
+++ tests/run_coverage.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import os
+import sys
 import inspect
 import coverage
 import subprocess
 
-# TODO: argument should be used to specify tests, py.test style
+print sys.argv[1:]
+arguments = ''
+if len(sys.argv) > 1:
+	arguments = ' '.join(sys.argv[1:])
 
 testdir = os.path.abspath(os.path.dirname(inspect.getfile(inspect.currentframe())))
 os.environ['COVERAGE_PROCESS_START'] = os.path.join(testdir, '.coveragerc')
@@ -13,7 +17,7 @@ cov = coverage.coverage(source=os.path.join(testdir, '..'),
 					include=os.path.join(testdir, '..', 'ofStateManager.py'))
 
 cov.erase()
-subprocess.call('coverage run -m py.test', shell=True, cwd=testdir)
+subprocess.call('coverage run -m py.test ' + arguments, shell=True, cwd=testdir)
 cov.combine()
 cov.html_report(directory=os.path.join(testdir, 'htmlcov'))
 cov.report(show_missing=False)
