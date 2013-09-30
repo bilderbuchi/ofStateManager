123f6a34d9d423f380254b70a5013c0df592d4b6
Christoph Buchner <bilderbuchi@phononoia.at>
2013-09-30 18:11:50 +0200

    Replace coverage API calls by subprocess calls.

--- tests/run_coverage.py
+++ tests/run_coverage.py
@@ -15,16 +15,19 @@ def main():
 		arguments = ' '.join(sys.argv[1:])
 	testdir = os.path.abspath(os.path.dirname(
 			inspect.getfile(inspect.currentframe())))
+	script_path = os.path.join(testdir, '..', 'ofStateManager.py')
+
 	os.environ['COVERAGE_PROCESS_START'] = os.path.join(testdir, '.coveragerc')
 	os.environ['COVERAGE_FILE'] = os.path.join(testdir, '.coverage')
-	cov = coverage.coverage(source=os.path.join(testdir, '..'),
-		include=os.path.join(testdir, '..', 'ofStateManager.py'))
-	cov.erase()
-	subprocess.call('coverage run -m py.test ' + arguments, shell=True,
-					cwd=testdir)
-	cov.combine()
-	cov.html_report(directory=os.path.join(testdir, 'htmlcov'))
-	cov.report(show_missing=False)
+
+	subprocess.call('coverage erase', shell=True, cwd=testdir)
+	subprocess.call('coverage run -m py.test ' + arguments, shell=True, cwd=testdir)
+	subprocess.call('coverage combine', shell=True, cwd=testdir)
+	subprocess.call('coverage html -d ' + os.path.join(testdir, 'htmlcov') +
+					' --include=' + script_path, shell=True, cwd=testdir)
+	subprocess.call('coverage report --include=' + script_path,
+					shell=True, cwd=testdir)
+
 
 if __name__ == '__main__':
 	main()
